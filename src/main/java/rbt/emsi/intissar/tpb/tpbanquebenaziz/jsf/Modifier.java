b0d5b58588b2d1d7dd778c784cc0ecb49542f871
Intissar BENAZIZ <Intissar BENAZIZ@DESKTOP-RRJUMKP>
2023-11-12 23:37:34 +0100

    modifier un compte

---
+++ src/main/java/rbt/emsi/intissar/tpb/tpbanquebenaziz/jsf/Modifier.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package rbt.emsi.intissar.tpb.tpbanquebenaziz.jsf;
+
+import jakarta.inject.Named;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import java.io.Serializable;
+import rbt.emsi.intissar.tpb.tpbanquebenaziz.entity.CompteBancaire;
+import rbt.emsi.intissar.tpb.tpbanquebenaziz.service.GestionnaireCompte;
+
+/**
+ *
+ * @author Intissar BENAZIZ
+ */
+@Named(value = "modifier")
+@ViewScoped
+public class Modifier implements Serializable {
+
+    private Long id;
+    private String nom;
+    private int solde;
+    private CompteBancaire compteBancaire;
+
+    @Inject
+    private GestionnaireCompte gestionnaireCompte;
+
+    /**
+     * Creates a new instance of Modifier
+     */
+    public Modifier() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getSolde() {
+        return solde;
+    }
+
+    public void setSolde(int solde) {
+        this.solde = solde;
+    }
+
+    public CompteBancaire getCompte() {
+        return compteBancaire;
+    }
+
+    public void loadCompte() {
+        compteBancaire = gestionnaireCompte.getCompte(id);
+    }
+
+    public String modif() {
+        compteBancaire.setNom(nom);
+        compteBancaire.setSolde(solde);
+        gestionnaireCompte.update(compteBancaire);
+        Util.addFlashInfoMessage("Nom modifié avec succès : " + nom);
+        return "listeComptes?faces-redirect=true";
+    }
+}
